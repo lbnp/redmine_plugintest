66ed4edd094ac92a7ce29f3f1674d2600fdd8799
Jun Kamoshima <kamoshima@gmail.com>
2014-07-25 22:21:20 +0900

    updated author information

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_plugintest do
   name 'Redmine Plugintest plugin'
-  author 'Author name'
+  author 'Jun Kamoshima'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/lbnp/redmine_plugintest'
+  author_url 'http://lbnp.github.io'
 end
